87cd91715f662ee6eb84473b821097e129b7dce5
Nikhil CSB <csbnikhil@gmail.com>
2020-12-05 00:06:45 +0530

    part two day three

--- trees/__init__.py
+++ trees/__init__.py
@@ -1,11 +1,18 @@
-def solve(geo):
+def get_tree_count(geo, step_right, step_down):
     trees = 0
     cols = len(geo[0])
     start = 1
 
-    for i, row in enumerate(geo):
-        trees += 1 if row[(i * 3) % cols] == '#' else 0
-
+    for i, row in enumerate(geo[::step_down]):
+        trees += 1 if row[(i * step_right) % cols] == '#' else 0
 
     return trees
+
+def solve(geo):
+    product = 1
+
+    for pair in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)):
+        product *= get_tree_count(geo, *pair)
+
+    return product
 
