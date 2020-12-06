365af2347e99664bab53c9cfb455cbc260434564
Nikhil CSB <csbnikhil@gmail.com>
2020-12-06 14:44:55 +0530

    day six part two

--- customs/__init__.py
+++ customs/__init__.py
@@ -1,7 +1,14 @@
-def solve(answers):
+from functools import reduce
+import operator
+
+def get_yes_count(answers):
     return sum(map(lambda x: len(set(''.join(x))), answers))
 
+def get_universal_answer(group):
+    return reduce(operator.and_, group)
 
+def solve(answers):
+    return sum(map(lambda x: len(get_universal_answer(list(map(set, x)))), answers))
 
 def read_input(file):
     with open(file) as f:
