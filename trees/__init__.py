cd98947f7920bd65ebdafc5d5ee7761afbd6d7c2
Nikhil CSB <csbnikhil@gmail.com>
2020-12-15 18:52:57 +0530

    one liner

--- trees/__init__.py
+++ trees/__init__.py
@@ -1,17 +1,17 @@
-def get_tree_count(geo, step_right, step_down):
-    trees = 0
-    cols = len(geo[0])
+from math import prod
 
-    for i, row in enumerate(geo[::step_down]):
-        trees += 1 if row[(i * step_right) % cols] == '#' else 0
+def read_input(file):
+    return list(map(lambda x: x.rstrip(), file.readlines()))
 
-    return trees
+def get_tree_count(geo, step_right, step_down, cols):
+    return sum(int(row[(step_right * i) % cols]) for i, row in enumerate(list(map(lambda x: x.replace('#', '1').replace('.', '0'), geo))[step_down::step_down], start=1))
 
 def solve(geo):
     product = 1
+    cols = len(geo[0])
 
-    for pair in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)):
-        product *= get_tree_count(geo, *pair)
+    for pair in ((3, 1), (1, 1), (5, 1), (7, 1), (1, 2)):
+        product *= get_tree_count(geo, *pair, cols)
 
     return product
 
