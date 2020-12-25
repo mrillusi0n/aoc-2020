7e3f66d346c55a35d963b9d52db5de634d74b7b0
Nikhil CSB <csbnikhil@gmail.com>
2020-12-25 23:57:18 +0530

    day twelve part two

--- encoding/__init__.py
+++ encoding/__init__.py
@@ -9,30 +9,33 @@ def get_nums(nums, num):
 
     return 0
 
-def get_rule_breaker(nums):
-    for i, num in enumerate(nums[5:]): # FIXME create a param for the start idx
-        if not get_nums(nums[i:i+5], num):
+def get_rule_breaker(nums, count):
+    for i, num in enumerate(nums[count:]):
+        if not get_nums(nums[i:i+count], num):
             return num
 
     return -1
 
 def break_encryption(nums):
-    breaker = get_rule_breaker(nums)
+    breaker = get_rule_breaker(nums, 25)
     running_sum = 0
     block = []
 
+    print(breaker)
+
     for num in nums:
-        if running_sum == breaker:
-            break
-        if running_sum + num > breaker:
-            running_sum -= block[0]
-            del block[0]
-        
         running_sum += num
+
         block.append(num)
 
-    return min(block) + max(block)
+        while running_sum > breaker:
+            running_sum -= block.pop(0)
 
+        if running_sum == breaker:
+            break
+        
+
+    return min(block) + max(block), sum(block) == breaker
 
 def solve(i):
     return break_encryption(i)
