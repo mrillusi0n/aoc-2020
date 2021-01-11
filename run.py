59384487d6040c5058458bbd3c8b1ed8593ce5d7
Nikhil CSB <csbnikhil@gmail.com>
2021-01-11 15:07:37 +0530

    delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from shuttle_search import solve, read_input
-
-def main():
-    with open('shuttle_search/input.txt') as f:
-        tests = read_input(f)
-        print(solve(tests))
-
-if __name__ == '__main__':
-    main()
