51953fc3d4168ed1572187e90faab798120b3e0b
Nikhil CSB <csbnikhil@gmail.com>
2021-01-11 21:17:08 +0530

    rewrite to handle parts

--- 4_passports/__init__.py
+++ 4_passports/__init__.py
@@ -1,5 +1,10 @@
 import re
 
+def parse_input(text):
+    return list(map(dict,
+                map(lambda props: map(lambda prop: prop.split(':'), props),
+                map(str.split, text.split('\n\n')))))
+
 def validate_byr(byr):
     return 1920 <= int(byr) <= 2002
 
@@ -36,19 +41,11 @@ def validate_ecl(ecl):
 def validate_pid(pid):
     return re.search(r'^\d{9}$', pid)
 
-
 def is_valid(passport):
-    required = {
-        'byr': validate_byr,
-        'iyr': validate_iyr,
-        'eyr': validate_eyr,
-        'hgt': validate_hgt,
-        'hcl': validate_hcl,
-        'ecl': validate_ecl,
-        'pid': validate_pid,
-    }
+    return (REQUIRED.keys() - passport.keys()) == set()
 
-    for field, validator in required.items():
+def is_still_valid(passport):
+    for field, validator in REQUIRED.items():
         if val := passport.get(field):
             if validator(val):
                 continue
@@ -56,11 +53,22 @@ def is_valid(passport):
 
     return True
 
-def solve(docs):
-    passports = []
 
-    for doc in docs:
-        passports.append({k: v for k, v in map(lambda x: x.split(':'), doc.split())})
+REQUIRED = {
+    'byr': validate_byr,
+    'iyr': validate_iyr,
+    'eyr': validate_eyr,
+    'hgt': validate_hgt,
+    'hcl': validate_hcl,
+    'ecl': validate_ecl,
+    'pid': validate_pid,
+}
 
-    return len(list(filter(is_valid, passports)))
+def solve(docs, parts):
+    solvers = {
+        1: sum(map(is_valid, docs)),
+        2: sum(map(is_still_valid, docs)),
+    }
+
+    return list(map(solvers.get, parts))
 
